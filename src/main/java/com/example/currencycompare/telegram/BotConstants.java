5b08abdbe849441f15a267868ef37757503c30bc
VikMoto <117093117+VikMoto@users.noreply.github.com>
2023-03-17 23:29:30 +0200

    currency-compare bot

--- src/main/java/com/example/currencycompare/telegram/BotConstants.java
+++ src/main/java/com/example/currencycompare/telegram/BotConstants.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class BotConstants {
-    private static final String CREDENTIALS_FILE = "src/main/resources/bot_credentials.txt";
+    private static final String CREDENTIALS_FILE = "/home/ubuntu/currency-compare/build/libs/src/main/resources/bot_credentials.txt";
 //    private static final String CREDENTIALS_FILE = "src\\main\\resources\\bot_credentials.txt";
 //    private static final String CREDENTIALS_FILE = "E:\\Business\\Code\\Projects\\currency-copmpare\\src\\main\\resources\\bot_credentials.txt";
     private static final Properties properties = new Properties();
